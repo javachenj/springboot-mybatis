24be8c89f686e85f8138c9de6b064754243e6d0e
chenj <chenj@163.com>
2020-07-04 23:48:34 +0800

    GitTest 代码修改 提交

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("112333aaaa");
         System.out.println("112333bbbbb");
+        System.out.println("avasfdasadds...");
     }
 }
